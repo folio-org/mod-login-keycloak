9e07d41f4a57dea374426dee8bbb464cde44bdcf
Taras Spashchenko <41001394+TarasSpashchenko@users.noreply.github.com>
2024-09-06 10:01:47 +0000

    Bump up applications-poc-tools dependencies to 1.5.6 to support Hostname Verification for TLS connections (#89)

--- src/main/java/org/folio/login/integration/keycloak/config/KeycloakAdminConfiguration.java
+++ src/main/java/org/folio/login/integration/keycloak/config/KeycloakAdminConfiguration.java
@@ -2,10 +2,12 @@ package org.folio.login.integration.keycloak.config;
 
 import static jakarta.ws.rs.client.ClientBuilder.newBuilder;
 import static org.apache.commons.lang3.StringUtils.stripToNull;
-import static org.apache.http.conn.ssl.NoopHostnameVerifier.INSTANCE;
-import static org.folio.common.utils.FeignClientTlsUtils.buildSslContext;
+import static org.folio.common.utils.tls.FeignClientTlsUtils.buildSslContext;
+import static org.folio.common.utils.tls.Utils.IS_HOSTNAME_VERIFICATION_DISABLED;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.http.conn.ssl.DefaultHostnameVerifier;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.folio.common.configuration.properties.TlsProperties;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.keycloak.OAuth2Constants;
@@ -19,6 +21,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties({KeycloakProperties.class})
 public class KeycloakAdminConfiguration {
+
+  private static final DefaultHostnameVerifier DEFAULT_HOSTNAME_VERIFIER = new DefaultHostnameVerifier();
 
   @Bean
   public Keycloak keycloak(KeycloakProperties properties) {
@@ -40,6 +44,9 @@ public class KeycloakAdminConfiguration {
   }
 
   private static ResteasyClient buildResteasyClient(TlsProperties properties) {
-    return (ResteasyClient) newBuilder().sslContext(buildSslContext(properties)).hostnameVerifier(INSTANCE).build();
+    return (ResteasyClient) newBuilder()
+      .sslContext(buildSslContext(properties))
+      .hostnameVerifier(IS_HOSTNAME_VERIFICATION_DISABLED ? NoopHostnameVerifier.INSTANCE : DEFAULT_HOSTNAME_VERIFIER)
+      .build();
   }
 }
