63e95385f3091c8c10d945c6d839bdde39c43868
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2025-09-15 11:46:52 +0300

    MODLOGINKC-48 - Use SECURE_STORE_ENV, not ENV, for secure store key (#155)
    * Revert "Revert "MODLOGINKC-48 - Use SECURE_STORE_ENV, not ENV, for secure store key (#152)" (#153)"
    
    This reverts commit 81f6b7f89369f67990281ded8540cd17c0444845.
    
    * MODLOGINKC-48 - read env value from secureStoreProperties
    
    (cherry picked from commit 4c908c4197b7387ee39bccaa2d4e3bbeabaf3db6)
    
    # Conflicts:
    #	NEWS.md
    #	src/main/java/org/folio/login/service/RealmConfigurationProvider.java
    #	src/test/java/org/folio/login/service/RealmConfigurationProviderTest.java

--- src/main/java/org/folio/login/service/RealmConfigurationProvider.java
+++ src/main/java/org/folio/login/service/RealmConfigurationProvider.java
@@ -1,12 +1,12 @@
 package org.folio.login.service;
 
 import lombok.RequiredArgsConstructor;
-import org.folio.common.configuration.properties.FolioEnvironment;
 import org.folio.login.domain.model.KeycloakRealmConfiguration;
 import org.folio.login.integration.keycloak.config.KeycloakProperties;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.tools.store.SecureStore;
 import org.folio.tools.store.exception.NotFoundException;
+import org.folio.tools.store.properties.SecureStoreProperties;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +15,9 @@ import org.springframework.stereotype.Service;
 public class RealmConfigurationProvider {
 
   private final SecureStore secureStore;
-  private final FolioEnvironment folioEnvironment;
   private final FolioExecutionContext folioExecutionContext;
   private final KeycloakProperties keycloakProperties;
+  private final SecureStoreProperties secureStoreProperties;
 
   /**
    * Provides realm configuration using {@link FolioExecutionContext} object.
@@ -35,7 +35,7 @@ public class RealmConfigurationProvider {
 
   private String retrieveKcClientSecret(String tenantId, String clientId) {
     try {
-      return secureStore.get(buildKey(folioEnvironment.getEnvironment(), tenantId, clientId));
+      return secureStore.get(buildKey(secureStoreProperties.getEnvironment(), tenantId, clientId));
     } catch (NotFoundException e) {
       throw new IllegalStateException(String.format(
         "Failed to get value from secure store [tenantId: %s, clientId: %s]", tenantId, clientId), e);
